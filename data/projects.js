eecccc8698b270d529c0349d05b329ed9f98a1a4
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2019-10-03 09:28:13 +0800

    Add screenshots to project data

--- data/projects.js
+++ data/projects.js
@@ -6,6 +6,18 @@ export default [
     repository: 'https://github.com/rvvergara/facialbook',
     demoLink: 'https://facials.herokuapp.com/',
     technologies: ['ruby', 'ruby on rails', 'jquery', 'bootstrap'],
+    description: `
+      <p>Attempted to clone Facebook here using Rails.</p>
+      <p>It was an awesome project!</p>
+    `,
+    screenshots: [
+      'http://tinyimg.io/i/lej8jW1.png',
+      'http://tinyimg.io/i/ejAbYVz.png',
+      'http://tinyimg.io/i/1JpQqAQ.png',
+      'http://tinyimg.io/i/vasZvJJ.png',
+      'http://tinyimg.io/i/YhQUQdL.png',
+      'http://tinyimg.io/i/iUsuM4i.png',
+    ],
   },
   {
     id: 2,
@@ -14,5 +26,17 @@ export default [
     repository: 'https://github.com/rvvergara/bookstore-cms-next-app',
     demoLink: 'https://bookstore-cms-next-app.herokuapp.com',
     technologies: ['next js', 'react', 'redux', 'jest', 'sass'],
+    description: `
+      <p>This used React and Redux</p>
+      <p>People can sign up and read books</p>
+    `,
+    screenshots: [
+      'http://tinyimg.io/i/74fQfGQ.png',
+      'http://tinyimg.io/i/mRSiOCF.png',
+      'http://tinyimg.io/i/hyFbvnR.png',
+      'http://tinyimg.io/i/ALEFczR.png',
+      'http://tinyimg.io/i/LJqjq64.png',
+      'http://tinyimg.io/i/7aFNdnF.png',
+    ],
   },
 ];
