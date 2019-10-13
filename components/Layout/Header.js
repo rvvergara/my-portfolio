baef584cf318431e4dd0b7fedf503606f9557ad8
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2019-10-13 13:45:58 +0800

    Refine style for Header brand and navbar collapse

--- components/Layout/Header.js
+++ components/Layout/Header.js
@@ -1,5 +1,7 @@
 import Link from 'next/link';
 import Container from 'react-bootstrap/Container';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
 import Navbar from 'react-bootstrap/Navbar';
 import MainNav from './MainNav';
 
@@ -11,9 +13,14 @@ const Header = ({ activeKey }) => (
     <Container>
       <Link href="/">
         <Navbar.Brand href="/">
+          <Row>
+            <Col sm={6}>
             Ryan Vergara
-          {' '}
-          <span className="subtitle">Fullstack Developer</span>
+            </Col>
+            <Col sm={6}>
+              <span className="subtitle">Fullstack Developer</span>
+            </Col>
+          </Row>
         </Navbar.Brand>
       </Link>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
