73d561635f913f6009e4045d59f0342c134b694a
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2019-10-14 12:07:30 +0800

    Add minimal styling for Modal

--- components/Projects/ProjectModal.js
+++ components/Projects/ProjectModal.js
@@ -1,15 +1,28 @@
 import PropTypes from 'prop-types';
+import { ReactHTMLConverter } from 'react-html-converter/node';
 import Modal from 'react-bootstrap/Modal';
+
+const Test = ({ text }) => (
+  <div>{text}</div>
+);
+
+const converter = new ReactHTMLConverter();
+converter.registerComponent('test', Test);
 
 const ProjectModal = ({ projectShown, handleClose, isShown }) => (
   <Modal
     show={isShown}
     onHide={handleClose}
+    size="xl"
   >
     <Modal.Header closeButton>
       <Modal.Title>{projectShown.title}</Modal.Title>
     </Modal.Header>
-    <Modal.Body>Project details here</Modal.Body>
+    <Modal.Body>
+      {
+        converter.convert(projectShown.description)
+      }
+    </Modal.Body>
   </Modal>
 );
 
@@ -17,6 +30,10 @@ ProjectModal.propTypes = {
   handleClose: PropTypes.func.isRequired,
   isShown: PropTypes.bool.isRequired,
   projectShown: PropTypes.instanceOf(Object).isRequired,
+};
+
+Test.propTypes = {
+  text: PropTypes.string.isRequired,
 };
 
 export default ProjectModal;
