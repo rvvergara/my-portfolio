f6091f6ec26d6e5d927242e04b3e158013685227
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2019-10-14 10:44:36 +0800

    Structure ProjectList and Project components

--- components/Projects/Project.js
+++ components/Projects/Project.js
@@ -1,9 +1,22 @@
+import { ReactHTMLConverter } from 'react-html-converter/node';
 import PropTypes from 'prop-types';
+import Card from 'react-bootstrap/Card';
+
+const Test = ({ text }) => (
+  <div>{text}</div>
+);
+
+const converter = new ReactHTMLConverter();
+converter.registerComponent('test', Test);
 
 const Project = ({ project }) => (
-  <div>
-    {project.title}
-  </div>
+  <Card>
+    <Card.Img variant="top" src={project.screenshots[0].photo} />
+    <Card.Title>{project.title}</Card.Title>
+    <Card.Text>
+      {converter.convert(project.description)}
+    </Card.Text>
+  </Card>
 );
 
 Project.propTypes = {
--- components/Projects/ProjectList.js
+++ components/Projects/ProjectList.js
@@ -1,8 +1,16 @@
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 import Project from './Project';
 import projects from '../../data/projects';
 
-const ProjectList = () => projects.map(project => (
-  <Project project={project} key={project.id} />
-));
+const ProjectList = () => (
+  <Row>
+    {projects.map(project => (
+      <Col key={project.id} lg={4}>
+        <Project project={project} />
+      </Col>
+    ))}
+  </Row>
+);
 
 export default ProjectList;
