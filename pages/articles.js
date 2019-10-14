8927c6f20379b41f78ed405db00f4082ea21634a
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2019-10-14 10:09:10 +0800

    Build Articles and Contact pages

---
+++ pages/articles.js
@@ -0,0 +1,8 @@
+import Layout from '../components/Layout';
+
+const ArticlesPage = () => (
+  <Layout title="Articles">
+    <h1>My Articles here</h1>
+  </Layout>
+);
+export default ArticlesPage;
---
+++ pages/contact.js
@@ -0,0 +1,8 @@
+import Layout from '../components/Layout';
+
+const ContactPage = () => (
+  <Layout title="Contact">
+    <h1>My Contact Info Here</h1>
+  </Layout>
+);
+export default ContactPage;
