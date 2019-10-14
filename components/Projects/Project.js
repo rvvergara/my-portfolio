f4275a82ddd1600750b8e1d70cc7290cb7fd59f7
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2019-10-14 11:14:59 +0800

    Create reducer for projectShown
    - reducer is to determine which project will be shown in a modal
    - pass dispatch as a prop to Project component from ProjectList

--- components/Projects/Project.js
+++ components/Projects/Project.js
@@ -9,15 +9,24 @@ const Test = ({ text }) => (
 const converter = new ReactHTMLConverter();
 converter.registerComponent('test', Test);
 
-const Project = ({ project }) => (
-  <Card>
-    <Card.Img variant="top" src={project.screenshots[0].photo} />
-    <Card.Title>{project.title}</Card.Title>
-    <Card.Text>
-      {converter.convert(project.description)}
-    </Card.Text>
-  </Card>
-);
+const Project = ({ project, dispatch }) => {
+  const setProjectShown = () => {
+    dispatch({
+      type: 'SET_PROJECT',
+      project,
+    });
+  };
+
+  return (
+    <Card onClick={setProjectShown}>
+      <Card.Img variant="top" src={project.screenshots[0].photo} />
+      <Card.Title>{project.title}</Card.Title>
+      <div className="card-text">
+        {converter.convert(project.description)}
+      </div>
+    </Card>
+  );
+};
 
 Project.propTypes = {
   project: PropTypes.instanceOf(Object).isRequired,
--- components/Projects/ProjectList.js
+++ components/Projects/ProjectList.js
@@ -1,16 +1,22 @@
+import { useReducer } from 'react';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Project from './Project';
+import projectReducer from '../../reducers/project';
 import projects from '../../data/projects';
 
-const ProjectList = () => (
-  <Row>
-    {projects.map(project => (
-      <Col key={project.id} lg={4}>
-        <Project project={project} />
-      </Col>
-    ))}
-  </Row>
-);
+const ProjectList = () => {
+  const [projectShown, dispatch] = useReducer(projectReducer, null);
+  console.log(projectShown);
+  return (
+    <Row>
+      {projects.map(project => (
+        <Col key={project.id} lg={4}>
+          <Project project={project} dispatch={dispatch} />
+        </Col>
+      ))}
+    </Row>
+  );
+};
 
 export default ProjectList;
