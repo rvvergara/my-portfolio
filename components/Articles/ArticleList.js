a286556091c294fd2ac2c9540daf770f10e5e540
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2019-10-15 18:55:56 +0800

    Transfer article into Article component

--- components/Articles/ArticleList.js
+++ components/Articles/ArticleList.js
@@ -1,31 +1,13 @@
 import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import Image from 'react-bootstrap/Image';
 import articles from '../../data/articles';
+import Article from './Article';
 
 const ArticleList = () => (
   <Container>
     <h2 className="page-title">Articles</h2>
     {
       articles.map(article => (
-        <Row key={article.id} className="article">
-          <Col className="article-img">
-            <a href={article.link}><Image src={article.pic} fluid /></a>
-          </Col>
-          <Col className="article-details">
-            <h4 className="article-title">
-              <a href={article.link}>{article.title}</a>
-            </h4>
-            <p className="article-excerpt">
-              {article.excerpt}
-              {' '}
-              <strong className="article-link">
-                <a href={article.link}>{`Read more from ${article.host}`}</a>
-              </strong>
-            </p>
-          </Col>
-        </Row>
+        <Article key={article.id} article={article} />
       ))
     }
   </Container>
