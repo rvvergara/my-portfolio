5a1c736cf59d13aadafc908716665cd4230797bd
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2019-10-15 17:07:28 +0800

    Create data file for articles

---
+++ data/articles.js
@@ -0,0 +1,33 @@
+export default [
+  {
+    id: 1,
+    title: 'How Five Weeks of Remote Pair Programming Helped me Build Strong Habits',
+    link: 'https://medium.com/hackernoon/how-five-weeks-of-remote-pair-programming-helped-me-build-strong-habits-e0493c9ba780',
+    pic: 'https://miro.medium.com/max/4000/1*G7pEtDLtgCvfb3ryFNTfWg.jpeg',
+    excerpt: 'Thanks to the Internet, opportunities that used to be limited by geography now are available to everyone. However, it also means learning how to work with others in a different part of the world. Cultural differences, at times language barriers, and even attitude clashes can break team members working remotely with each other.',
+    host: 'Hackernoon Medium Publication',
+  },
+  {
+    id: 2,
+    title: 'How Much HTML and CSS Should You Know To Be A Complete Developer?',
+    link: 'https://medium.com/hackernoon/how-much-html-and-css-should-you-know-to-develop-4456e2ca8595',
+    pic: 'https://miro.medium.com/max/6000/1*sRogZ0gsnPGpi20FcakCzw.jpeg',
+    excerpt: 'These days we are blessed to have tools, libraries and frameworks to make it simpler and easier for us to build the markup and styles of a site. It was previously tough to perfect a page’s layout using purely floats and position properties in CSS. But now we have FlexBox and CSS Grid. Having basic skills in those two will already allow one to make incredible looking web pages.',
+    host: 'Hackernoon Medium Publication',
+  },
+  {
+    id: 3,
+    title: 'How to calculate a Binary Tree’s height using array iteration in Ruby',
+    link: 'https://www.freecodecamp.org/news/how-to-calculate-a-binary-trees-height-using-array-iteration-in-ruby-63551c6c65fe/',
+    pic: 'https://cdn-media-1.freecodecamp.org/images/1*HItcyUcWWnuqDzKgNCoprw.jpeg',
+    excerpt: 'If you observe the directories or file structure in your computer, it follows a (albeit the more general) tree structure. Each folder can contain files (the data) and a number of other directories (which are not necessarily data in themselves but rather just addresses of such data contained within those sub directories).',
+    host: 'FreeCodeCamp News',
+  },
+  {
+    id: 4,
+    title: 'How to calculate Binary Tree height with the recursive method',
+    link: 'https://www.freecodecamp.org/news/how-to-calculate-binary-tree-height-with-the-recursive-method-aafc461f2201/',
+    pic: 'https://cdn-media-1.freecodecamp.org/images/1*tsVyCA_zXrAh3LqTF4LHxw.png',
+    excerpt: 'Previously I wrote about an algorithm for finding out the height of a binary tree using iteration. Though that method gets the job done (in 7 steps no less), the same can be accomplished in a much simpler way. In my opinion, one of the most powerful programming techniques is recursion. For readers new to programming — it is simply a function or method calling itself. ',
+  },
+];
