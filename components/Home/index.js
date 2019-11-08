91ecff72f1909c2559d4c3a4e5ebfbc9b64ef65b
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2019-11-08 17:28:58 +0800

    Fix linter errors in Home component

--- components/Home/index.js
+++ components/Home/index.js
@@ -1,28 +1,31 @@
 import Jumbotron from 'react-bootstrap/Jumbotron';
-import Card from 'react-bootstrap/Card';
-import homeContent from '../../data/home';
 
 const HomePage = () => (
   <div>
     <Jumbotron fluid className="problem-intro">
-      <h1>Let's Bring Your Ideas To Life</h1>
+      <h1>Let&apos;s Bring Your Ideas To Life</h1>
       <p>
-        Are you a startup building something that can change the world? Are you searching for someone who can help you shape your vision and bring it into reality?
+        Are you a startup building something that can change the world?
+        Are you searching for someone who can help you shape your vision and bring it into reality?
       </p>
       <p>
-        You may be in the stage of building your MVP or are adding new features to your product. In both cases, you need a partner that is passionate about your technology and your vision.
+        You may be in the stage of building your MVP or are adding new features to your product.
+        In both cases, you need a partner that is passionate about your technology and your vision.
       </p>
     </Jumbotron>
     <Jumbotron fluid className="my-intro">
-      <h2>Hello, I'm Ryan</h2>
+      <h2>Hello, I&apos;m Ryan</h2>
       <p>
         I have built awesome applications using Ruby on Rails, NodeJS, NextJS and React.
       </p>
       <p>
-        From storyboarding, to mockup design, data-modelling up until deployment, I love working on ideas.
+        From storyboarding, to mockup design, data-modelling up until deployment,
+        I love working on ideas.
       </p>
       <p>
-      At the moment I am adding Machine Learning to my repertoire in order to build more complex apps that can accomplish diverse tasks for its users. Needless to say, I am truly passionate about technology!
+      At the moment I am adding Machine Learning to my repertoire
+      in order to build more complex apps that can accomplish diverse tasks for its users.
+      Needless to say, I am truly passionate about technology!
       </p>
     </Jumbotron>
   </div>
